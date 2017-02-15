ac3b1093fd136077a83ea5db25938a44fd69ed96
Derek Boytim <boytim.2@gmail.com>
2017-02-14 21:08:38 -0500

    Initialized an empty universe

--- src/main/java/GameOfLIfe.java
+++ src/main/java/GameOfLIfe.java
@@ -2,6 +2,22 @@
  * Created by derek on 2/14/17.
  */
 public class GameOfLIfe {
+    private boolean ready;
+    private boolean[][] universe;
+
+    public GameOfLIfe() {
+        ready = init();
+    }
+
+    private boolean init() {
+        universe = new boolean[8][6];
+        return true;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
     public static void main(String[] args) {
 
     }
